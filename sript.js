9f52c1ef6137daec10a97209d7e44a133a5460fb
Lynette Jane A. Bojocan <lynettejane0923@gmail.com>
2023-11-08 23:58:59 +0800

    update

--- sript.js
+++
@@ -1,9 +0,0 @@
-// Get the dropdown element
-var dropdown = document.getElementById("myDropdown");
-
-// Add an event listener to listen for changes
-dropdown.addEventListener("change", function() {
-    var selectedOption = dropdown.value;
-    // Do something with the selected option
-    console.log("Selected option: " + selectedOption);
-});
