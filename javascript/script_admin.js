777d51f16eef34b3fc29578e18ea3941261ef582
lynette1105 <lynettejane0923@gmail.com>
2023-11-17 21:36:38 +0800

    admin update

--- javascript/script_admin.js
+++ javascript/script_admin.js
@@ -1,9 +1,18 @@
+    // Dash board//
+const lastSelectedSection = localStorage.getItem('lastSelectedSection');
+    if (lastSelectedSection) {
+        showSection(lastSelectedSection);
+  }
 function showSection(sectionId) {
-    // Hide all sections
-        document.querySelectorAll('section').forEach(function(section) {
-        section.classList.remove('show-dashboard', 'show-messages', 'show-staff', 'show-memos', 'show-promotions');
-          });
-    
-    // Show the selected section
-        document.getElementById(sectionId).classList.add('show-' + sectionId);
-    }
+    document.querySelectorAll('section').forEach(function(section) {
+    section.classList.remove('show-dashboard', 'show-messages', 'show-staff', 'show-memos', 'show-promotions', 'show-interactives', 'show-subscriptions',);
+        });
+
+    document.getElementById(sectionId).classList.add('show-' + sectionId);
+    localStorage.setItem('lastSelectedSection', sectionId);
+}
+
+    //popover//
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+    });
